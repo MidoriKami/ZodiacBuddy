6affa7412aa5112605afc811d601fc64164c3877
Raymond <daemitus@gmail.com>
2022-07-03 08:53:49 -0400

    This might work

---
+++ ZodiacBuddy/Secrets.cs
@@ -0,0 +1,12 @@
+﻿namespace ZodiacBuddy;
+
+/// <summary>
+/// Secret management.
+/// </summary>
+internal static class Secrets
+{
+    /// <summary>
+    /// JWT secret for ZodiacDB.
+    /// </summary>
+    public const string JwtSecret = "$$JWT_TOKEN$$";
+}
