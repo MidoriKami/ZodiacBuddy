0726f470b2522b94d6d836f16897ff981c8c903f
Raymond <daemitus@gmail.com>
2022-07-18 19:32:47 -0400

    Namespacing

--- ZodiacBuddy/InformationWindow/InformationWindow.cs
+++ ZodiacBuddy/InformationWindow/InformationWindow.cs
@@ -6,123 +6,123 @@ using FFXIVClientStructs.FFXIV.Client.Game;
 using ImGuiNET;
 using ZodiacBuddy.BonusLight;
 
-namespace ZodiacBuddy.InformationWindow
+namespace ZodiacBuddy.InformationWindow;
+
+/// <summary>
+/// Default information window.
+/// </summary>
+public abstract class InformationWindow
 {
     /// <summary>
-    /// Default information window.
+    /// Initializes a new instance of the <see cref="InformationWindow"/> class.
     /// </summary>
-    public abstract class InformationWindow
+    /// <param name="name">Name of the window.</param>
+    protected InformationWindow(string name)
     {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="InformationWindow"/> class.
-        /// </summary>
-        /// <param name="name">Name of the window.</param>
-        protected InformationWindow(string name)
+        this.Name = name;
+    }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether to show this window.
+    /// </summary>
+    public bool ShowWindow { get; set; } = false;
+
+    /// <summary>
+    /// Gets or sets the mainhand item.
+    /// </summary>
+    public InventoryItem MainhandItem { get; set; } = default;
+
+    /// <summary>
+    /// Gets or sets the offhand item.
+    /// </summary>
+    public InventoryItem OffhandItem { get; set; } = default;
+
+    private static BonusLightConfiguration BonusConfiguration => Service.Configuration.BonusLight;
+
+    private static InformationWindowConfiguration InfoWindowConfiguration => Service.Configuration.InformationWindow;
+
+    /// <summary>
+    /// Gets name of the information window.
+    /// </summary>
+    private string Name { get; }
+
+    /// <summary>
+    /// Draw information window.
+    /// </summary>
+    public void Draw()
+    {
+        if (!this.ShowWindow)
+            return;
+
+        var flags = ImGuiWindowFlags.NoFocusOnAppearing |
+                    ImGuiWindowFlags.NoTitleBar |
+                    ImGuiWindowFlags.NoScrollbar;
+
+        flags |= InfoWindowConfiguration.ClickThrough ? ImGuiWindowFlags.NoInputs : ImGuiWindowFlags.None;
+        if (ImGui.Begin(this.Name, flags))
         {
-            this.Name = name;
-        }
+            this.DisplayRelicInfo(this.MainhandItem);
+            this.DisplayRelicInfo(this.OffhandItem);
+            this.DisplayBonusLight();
 
-        /// <summary>
-        /// Gets or sets a value indicating whether to show this window.
-        /// </summary>
-        public bool ShowWindow { get; set; } = false;
-
-        /// <summary>
-        /// Gets or sets the mainhand item.
-        /// </summary>
-        public InventoryItem MainhandItem { get; set; } = default;
-
-        /// <summary>
-        /// Gets or sets the offhand item.
-        /// </summary>
-        public InventoryItem OffhandItem { get; set; } = default;
-
-        private static BonusLightConfiguration BonusConfiguration => Service.Configuration.BonusLight;
-
-        private static InformationWindowConfiguration InfoWindowConfiguration => Service.Configuration.InformationWindow;
-
-        /// <summary>
-        /// Gets name of the information window.
-        /// </summary>
-        private string Name { get; }
-
-        /// <summary>
-        /// Draw information window.
-        /// </summary>
-        public void Draw()
-        {
-            if (!this.ShowWindow)
-                return;
-            var flags = ImGuiWindowFlags.NoFocusOnAppearing |
-                        ImGuiWindowFlags.NoTitleBar |
-                        ImGuiWindowFlags.NoScrollbar;
-
-            flags |= InfoWindowConfiguration.ClickThrough ? ImGuiWindowFlags.NoInputs : ImGuiWindowFlags.None;
-            if (ImGui.Begin(this.Name, flags))
+            if (!InfoWindowConfiguration.ManualSize)
             {
-                this.DisplayRelicInfo(this.MainhandItem);
-                this.DisplayRelicInfo(this.OffhandItem);
-                this.DisplayBonusLight();
-
-                if (!InfoWindowConfiguration.ManualSize)
-                {
-                    ImGui.SetWindowSize(this.Name, Vector2.Zero);
-                }
+                ImGui.SetWindowSize(this.Name, Vector2.Zero);
             }
-
-            ImGui.End();
         }
 
-        /// <summary>
-        /// Display information about the specified relic.
-        /// </summary>
-        /// <param name="item">Relic to display.</param>
-        protected abstract void DisplayRelicInfo(InventoryItem item);
+        ImGui.End();
+    }
 
-        /// <summary>
-        /// Determine the size of the progress bar.
-        /// </summary>
-        /// <param name="relicName">Name of the relic.</param>
-        /// <returns>Vector2 of the determined size.</returns>
-        protected Vector2 DetermineProgressSize(string relicName)
+    /// <summary>
+    /// Display information about the specified relic.
+    /// </summary>
+    /// <param name="item">Relic to display.</param>
+    protected abstract void DisplayRelicInfo(InventoryItem item);
+
+    /// <summary>
+    /// Determine the size of the progress bar.
+    /// </summary>
+    /// <param name="relicName">Name of the relic.</param>
+    /// <returns>Vector2 of the determined size.</returns>
+    protected Vector2 DetermineProgressSize(string relicName)
+    {
+        if (!InfoWindowConfiguration.ProgressAutoSize)
+            return Vector2.Zero with { X = InfoWindowConfiguration.ProgressSize };
+
+        if (InfoWindowConfiguration.ManualSize ||
+            (BonusConfiguration.DisplayBonusDuty &&
+             BonusConfiguration.LightBonusTerritoryId != null))
+            return Vector2.Zero with { X = ImGui.GetContentRegionAvail().X - 1 };
+
+        var vector = ImGui.CalcTextSize(relicName) with { Y = 0f };
+
+        return vector.X < 80 ?
+            new Vector2(80, 0) :
+            vector;
+    }
+
+    private void DisplayBonusLight()
+    {
+        if (!BonusConfiguration.DisplayBonusDuty)
+            return;
+
+        if (BonusConfiguration.LightBonusDetection != null &&
+            BonusConfiguration.LightBonusTerritoryId != null)
         {
-            if (!InfoWindowConfiguration.ProgressAutoSize)
-                return Vector2.Zero with { X = InfoWindowConfiguration.ProgressSize };
+            var dutyName = BonusLightDuty.GetValue(BonusConfiguration.LightBonusTerritoryId.Value).DutyName
+                .Replace("Œ", "Oe")
+                .Replace("œ", "oe");
+            var detectionDate = BonusConfiguration.LightBonusDetection.Value.ToLocalTime().ToString("t");
 
-            if (InfoWindowConfiguration.ManualSize ||
-                (BonusConfiguration.DisplayBonusDuty &&
-                 BonusConfiguration.LightBonusTerritoryId != null))
-                return Vector2.Zero with { X = ImGui.GetContentRegionAvail().X - 1 };
+            ImGui.PushFont(UiBuilder.IconFont);
+            ImGui.PushStyleColor(ImGuiCol.Text, ImGuiColors.DalamudYellow);
+            ImGui.Text(FontAwesomeIcon.Lightbulb.ToIconString());
+            ImGui.PopStyleColor();
+            ImGui.PopFont();
 
-            var vector = ImGui.CalcTextSize(relicName) with { Y = 0f };
-
-            return vector.X < 80 ?
-                new Vector2(80, 0) :
-                vector;
-        }
-
-        private void DisplayBonusLight()
-        {
-            if (!BonusConfiguration.DisplayBonusDuty)
-                return;
-
-            if (BonusConfiguration.LightBonusDetection != null &&
-                BonusConfiguration.LightBonusTerritoryId != null)
-            {
-                var dutyName = BonusLightDuty.GetValue(BonusConfiguration.LightBonusTerritoryId.Value).DutyName
-                    .Replace("Œ", "Oe")
-                    .Replace("œ", "oe");
-                var detectionDate = BonusConfiguration.LightBonusDetection.Value.ToLocalTime().ToString("t");
-
-                ImGui.PushFont(UiBuilder.IconFont);
-                ImGui.PushStyleColor(ImGuiCol.Text, ImGuiColors.DalamudYellow);
-                ImGui.Text(FontAwesomeIcon.Lightbulb.ToIconString());
-                ImGui.PopStyleColor();
-                ImGui.PopFont();
-
-                ImGui.SameLine();
-                ImGui.Text($"{detectionDate} \"{dutyName}\"");
-            }
+            ImGui.SameLine();
+            ImGui.Text($"{detectionDate} \"{dutyName}\"");
         }
     }
 }
--- ZodiacBuddy/InformationWindow/InformationWindowConfiguration.cs
+++ ZodiacBuddy/InformationWindow/InformationWindowConfiguration.cs
@@ -1,49 +1,48 @@
 ﻿using System;
 
-namespace ZodiacBuddy.InformationWindow
+namespace ZodiacBuddy.InformationWindow;
+
+/// <summary>
+/// Configuration class for weapons information window.
+/// </summary>
+public class InformationWindowConfiguration
 {
     /// <summary>
-    /// Configuration class for weapons information window.
+    /// Default color for the bonus light information progress bar.
     /// </summary>
-    public class InformationWindowConfiguration
+    [NonSerialized]
+    private static readonly uint DefaultProgressColor = 0xFF943463;
+
+    /// <summary>
+    /// Gets or sets a value indicating whether to enable manual resizing of the weapons information window.
+    /// </summary>
+    public bool ManualSize { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether to enable click through the weapons information window.
+    /// </summary>
+    public bool ClickThrough { get; set; }
+
+    /// <summary>
+    /// Gets or sets the color of the progress bar on weapons information.
+    /// </summary>
+    public uint ProgressColor { get; set; } = DefaultProgressColor;
+
+    /// <summary>
+    /// Gets or sets a value indicating whether to resize automatically the progress bar.
+    /// </summary>
+    public bool ProgressAutoSize { get; set; } = true;
+
+    /// <summary>
+    /// Gets or sets a value indicating the size of the progress bar.
+    /// </summary>
+    public int ProgressSize { get; set; } = 130;
+
+    /// <summary>
+    /// Reset ProgressColor to it's default value.
+    /// </summary>
+    public void ResetProgressColor()
     {
-        /// <summary>
-        /// Default color for the bonus light information progress bar.
-        /// </summary>
-        [NonSerialized]
-        private static readonly uint DefaultProgressColor = 0xFF943463;
-
-        /// <summary>
-        /// Gets or sets a value indicating whether to enable manual resizing of the weapons information window.
-        /// </summary>
-        public bool ManualSize { get; set; }
-
-        /// <summary>
-        /// Gets or sets a value indicating whether to enable click through the weapons information window.
-        /// </summary>
-        public bool ClickThrough { get; set; }
-
-        /// <summary>
-        /// Gets or sets the color of the progress bar on weapons information.
-        /// </summary>
-        public uint ProgressColor { get; set; } = DefaultProgressColor;
-
-        /// <summary>
-        /// Gets or sets a value indicating whether to resize automatically the progress bar.
-        /// </summary>
-        public bool ProgressAutoSize { get; set; } = true;
-
-        /// <summary>
-        /// Gets or sets a value indicating the size of the progress bar.
-        /// </summary>
-        public int ProgressSize { get; set; } = 130;
-
-        /// <summary>
-        /// Reset ProgressColor to it's default value.
-        /// </summary>
-        public void ResetProgressColor()
-        {
-            this.ProgressColor = DefaultProgressColor;
-        }
+        this.ProgressColor = DefaultProgressColor;
     }
 }
